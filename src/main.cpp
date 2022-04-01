12e810a9f4587744e1a38bea5d1a77c1e5da2906
root <nwp2@illinois.edu>
2022-04-01 14:27:54 -0500

    graph construction

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<rapidcsv.h>
+#include "RoadGraph.h"
 
 int main(){
     rapidcsv::Document nodes("data/NA.cnode.txt", rapidcsv::LabelParams(-1,-1), rapidcsv::SeparatorParams(' '));
@@ -7,12 +8,9 @@ int main(){
     auto y_coords = nodes.GetColumn<double>(2);
 
     rapidcsv::Document edges("data/NA.cedge.txt", rapidcsv::LabelParams(-1,-1), rapidcsv::SeparatorParams(' '));
-    auto from = edges.GetColumn<double>(1);
-    auto to = edges.GetColumn<double>(2);
+    auto from = edges.GetColumn<size_t>(1);
+    auto to = edges.GetColumn<size_t>(2);
     auto length = edges.GetColumn<double>(3);
 
-
-    for(auto x : x_coords){
-        std::cout << x << '\n';
-    }
+    RoadGraph graph{x_coords, y_coords, from, to, length};
 }
