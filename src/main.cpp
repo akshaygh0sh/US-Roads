02865e01cef398b431f1e7245fd09b398ac73332
root <nwp2@illinois.edu>
2022-04-01 14:11:49 -0500

    read csv

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
+#include<rapidcsv.h>
 
 int main(){
-    std::cout << "asdf\n";
+    rapidcsv::Document nodes("data/NA.cnode.txt", rapidcsv::LabelParams(-1,-1), rapidcsv::SeparatorParams(' '));
+    auto x_coords = nodes.GetColumn<double>(1);
+    auto y_coords = nodes.GetColumn<double>(2);
+
+    rapidcsv::Document edges("data/NA.cedge.txt", rapidcsv::LabelParams(-1,-1), rapidcsv::SeparatorParams(' '));
+    auto from = edges.GetColumn<double>(1);
+    auto to = edges.GetColumn<double>(2);
+    auto length = edges.GetColumn<double>(3);
+
+
+    for(auto x : x_coords){
+        std::cout << x << '\n';
+    }
 }
