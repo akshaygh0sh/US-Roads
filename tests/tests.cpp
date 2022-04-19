03f13783dc951b6410dd84c74502a2caf116b3c9
akshaygh0sh <408.aagh0sh@gmail.com>
2022-04-19 01:29:27 -0400

    Added small test case for shortestSalesman

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -33,3 +33,25 @@ TEST_CASE("Shortest Path Small Simple", "[shortestPath]") {
   REQUIRE(path_10_18.first == actual_path_10_18);
   REQUIRE(path_10_18.second == 122.56);
 }
+
+TEST_CASE("Shortest Travelling Salesman Simple", "[shortestSalesman]") {
+  rapidcsv::Document nodes("data/small_tests_node.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto x_coords = nodes.GetColumn<double>(1);
+  auto y_coords = nodes.GetColumn<double>(2);
+
+  rapidcsv::Document edges("data/small_tests_edge.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto from = edges.GetColumn<size_t>(1);
+  auto to = edges.GetColumn<size_t>(2);
+  auto length = edges.GetColumn<double>(3);
+
+  RoadGraph graph{x_coords, y_coords, from, to, length};
+  auto salesman_17_12_15_19 = graph.shortestSalesman({17, 12, 15, 19}).value();
+  std::vector<size_t> actual_path_17_12_15_19 {17, 8, 13, 12, 4, 15, 7, 1, 19};
+
+  REQUIRE(salesman_17_12_15_19.first == actual_path_17_12_15_19);
+  REQUIRE(salesman_17_12_15_19.second == 100.1);
+}
