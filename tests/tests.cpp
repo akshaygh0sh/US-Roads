61e6dbe39fca890f0164a725947aad225d716b2c
akshaygh0sh <408.aagh0sh@gmail.com>
2022-04-22 15:44:11 -0400

    Added more test cases for Shortest Travelling Salesman A*

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,34 +4,30 @@
 #include "../src/RoadGraph.h"
 #include "catch.hpp"
 
-RoadGraph load_graph(std::string node, std::string edge){
-  rapidcsv::Document nodes(node.c_str(),
+TEST_CASE("Shortest Path Small Simple", "[shortestPath]") {
+  rapidcsv::Document nodes("data/small_tests_node.txt",
                            rapidcsv::LabelParams(-1, -1),
                            rapidcsv::SeparatorParams(' '));
   auto x_coords = nodes.GetColumn<double>(1);
   auto y_coords = nodes.GetColumn<double>(2);
 
-  rapidcsv::Document edges(edge.c_str(),
+  rapidcsv::Document edges("data/small_tests_edge.txt",
                            rapidcsv::LabelParams(-1, -1),
                            rapidcsv::SeparatorParams(' '));
   auto from = edges.GetColumn<size_t>(1);
   auto to = edges.GetColumn<size_t>(2);
   auto length = edges.GetColumn<double>(3);
 
-  return {x_coords, y_coords, from, to, length};
-}
-
-TEST_CASE("Shortest Path Small Simple", "[shortestPath]") {
-  auto graph = load_graph("data/small_tests_node.txt", "data/small_tests_edge.txt");
+  RoadGraph graph{x_coords, y_coords, from, to, length};
 
   // Vars are defined as path_start_end
-  auto path_14_13 = graph.shortestPathAStar(14, 13).value();
+  auto path_14_13 = graph.shortestPath(14, 13).value();
   std::vector<size_t> actual_path_14_13{14, 16, 3, 13};
 
   REQUIRE(path_14_13.first == actual_path_14_13);
   REQUIRE(path_14_13.second == 127.05);
 
-  auto path_10_18 = graph.shortestPathAStar(10, 18).value();
+  auto path_10_18 = graph.shortestPath(10, 18).value();
   std::vector<size_t> actual_path_10_18{10, 0, 1, 7, 5, 18};
 
   REQUIRE(path_10_18.first == actual_path_10_18);
@@ -39,12 +35,79 @@ TEST_CASE("Shortest Path Small Simple", "[shortestPath]") {
 }
 
 TEST_CASE("Shortest Travelling Salesman Simple", "[shortestSalesman]") {
-  auto graph = load_graph("data/small_tests_node.txt", "data/small_tests_edge.txt");
+  rapidcsv::Document nodes("data/small_tests_node.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto x_coords = nodes.GetColumn<double>(1);
+  auto y_coords = nodes.GetColumn<double>(2);
 
+  rapidcsv::Document edges("data/small_tests_edge.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto from = edges.GetColumn<size_t>(1);
+  auto to = edges.GetColumn<size_t>(2);
+  auto length = edges.GetColumn<double>(3);
+
+  RoadGraph graph{x_coords, y_coords, from, to, length};
   auto salesman_17_12_15_19 = graph.shortestSalesman({17, 12, 15, 19}).value();
-  std::vector<size_t> actual_path_17_12_15_19 {17, 2, 19, 1, 7, 15, 4, 12};
+  std::vector<size_t> actual_path_17_12_15_19{17, 2, 19, 1, 7, 15, 4, 12};
 
-  
   REQUIRE(salesman_17_12_15_19.first == actual_path_17_12_15_19);
   REQUIRE(salesman_17_12_15_19.second == 54.2415);
+}
+
+TEST_CASE("Shortest Travelling Salesman A* Simple", "[shortestSalesman]") {
+  rapidcsv::Document nodes("data/NA.cnode.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto x_coords = nodes.GetColumn<double>(1);
+  auto y_coords = nodes.GetColumn<double>(2);
+
+  rapidcsv::Document edges("data/NA.cedge.txt",
+                           rapidcsv::LabelParams(-1, -1),
+                           rapidcsv::SeparatorParams(' '));
+  auto from = edges.GetColumn<size_t>(1);
+  auto to = edges.GetColumn<size_t>(2);
+  auto length = edges.GetColumn<double>(3);
+
+  RoadGraph graph{x_coords, y_coords, from, to, length};
+  std::vector<size_t> path = {17, 12, 15, 19};
+  // Path
+  REQUIRE(graph.shortestSalesman(path).value().first ==
+          graph.shortestSalesmanAStar(path).value().first);
+  // Distance of Path
+  REQUIRE(graph.shortestSalesman(path).value().second ==
+          graph.shortestSalesmanAStar(path).value().second);
+
+  path = {1, 3, 5, 7, 9, 11};
+  // Path
+  REQUIRE(graph.shortestSalesman(path).value().first ==
+          graph.shortestSalesmanAStar(path).value().first);
+  // Distance of Path
+  REQUIRE(graph.shortestSalesman(path).value().second ==
+          graph.shortestSalesmanAStar(path).value().second);
+
+  path = {2, 4, 6, 8, 10, 12, 14};
+  // Path
+  REQUIRE(graph.shortestSalesman(path).value().first ==
+          graph.shortestSalesmanAStar(path).value().first);
+  // Distance of Path
+  REQUIRE(graph.shortestSalesman(path).value().second ==
+          graph.shortestSalesmanAStar(path).value().second);
+  
+  path = {1337, 430, 1392, 4389, 32132, 17834, 800};
+  // Path
+  REQUIRE(graph.shortestSalesman(path).value().first ==
+          graph.shortestSalesmanAStar(path).value().first);
+  // Distance of Path
+  REQUIRE(graph.shortestSalesman(path).value().second ==
+          graph.shortestSalesmanAStar(path).value().second);
+
+  path = {1337, 430, 1392, 4389, 32132, 17834, 800};
+  // Path
+  REQUIRE(graph.shortestSalesman(path).value().first ==
+          graph.shortestSalesmanAStar(path).value().first);
+  // Distance of Path
+  REQUIRE(graph.shortestSalesman(path).value().second ==
+          graph.shortestSalesmanAStar(path).value().second);
 }
